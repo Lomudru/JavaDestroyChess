260bad980e987d33c14f91d18faab1762740de25
Lomudru <kmejasson@gmail.com>
2024-02-13 15:19:50 +0100

    ajout de l'affichage des regles

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
             System.out.println("Les scores");
         } else if ((reponse.equals("3")) || reponse.toLowerCase().equals("règles")) {
             // Si la réponse est 3 ou règles, on affiche les règles
-            System.out.println("Les règles");
+            regles();
         } else if ((reponse.equals("4")) || reponse.toLowerCase().equals("quitter")) {
             // Si la réponse est 4 ou quitter, on dit au revoir et on arrête le code
             System.out.println("Au revoir");
@@ -39,5 +39,16 @@ public class Main {
         }
         // Et puis on rappelle la fonction
         menu(scanner);
+    }
+    /** Affiche les règles du jeu */
+    public static void regles()
+    {
+        System.out.println("------------------------------------------------------------------------------------------------");
+        System.out.println("| Un joueur ne peut pas se déplacé en diagonale.                                               |");
+        System.out.println("| Si le joueur n'a plus la possibilité de se déplacé autour de lui, il a perdu.                |");
+        System.out.println("| Le joueur peut détruire une case lorsqu'il s'est déplacé.                                    |");
+        System.out.println("| La partie se termine lorsqu'il reste un seul survivant                                       |");
+        System.out.println("| Un joueur gagne 5 point lorsqu'il est le dernier survivant et perd 2 point lorsqu'il meurt   |");
+        System.out.println("------------------------------------------------------------------------------------------------");
     }
 }
