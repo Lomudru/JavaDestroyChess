58033d98788314c5ba58ed2636d89a8788d0180d
Lomudru <kmejasson@gmail.com>
2024-02-14 10:10:37 +0100

    Ajout de la fonction pour afficher les joueurs sur le tableau

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
+import java.util.Random;
 import java.util.Scanner;
+import classe.Joueur;
+import javax.print.attribute.standard.JobOriginatingUserName;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] plateauDeJeu = {{0,0,0,0,0,0,0,0,0,0,0},
-                                {0,0,0,0,0,0,0,0,0,0,0},
-                                {0,0,0,0,0,0,0,0,0,0,0},
-                                {0,0,0,0,0,0,0,0,0,0,0},
-                                {0,0,0,0,0,2,0,0,0,0,0},
-                                {0,0,0,0,0,3,0,0,0,0,0},
-                                {0,0,0,0,0,0,0,0,0,0,0},
-                                {0,0,0,0,0,0,0,0,0,0,0},
-                                {0,0,0,0,0,0,0,0,0,0,0},
-                                {0,0,0,0,0,0,0,0,0,0,0}};
-        afficherTableau(plateauDeJeu);
         // Affiche le titre du jeu
         System.out.println("\n" +
                 "████████▄     ▄████████    ▄████████     ███        ▄████████  ▄██████▄  ▄██   ▄    ▄████████    ▄█    █▄       ▄████████    ▄████████    ▄████████ \n" +
@@ -46,8 +38,18 @@ public class Main {
         // Demande à l'utilisateur de choisir une option
         String reponse = scanner.nextLine();
         if ((reponse.equals("1")) || reponse.toLowerCase().equals("jouer")) {
-            // Si la réponse est 1 ou jouer, on envoie vers le jeu
-            System.out.println("Jouer bien");
+            // Si la réponse est 1 ou jouer, on envoie vers le jeu et génère le plateau
+            int[][] plateauDeJeu = {{0,0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0,0}};
+            Jeu(plateauDeJeu);
         } else if ((reponse.equals("2")) || reponse.toLowerCase().equals("scores")) {
             // Si la réponse est 2 ou scores, on affiche les scores
             System.out.println("Les scores");
@@ -82,22 +84,61 @@ public class Main {
         //initialise une varibale nb pour les Colones et les Lignes
         int nbLignes=Tableau.length;
         int nbColones=Tableau[0].length;
+        String[] Alphabet = {"  A ","B ","C ","D","E ","F ","H ","I ","J ","K","L"};
+        for (int idLettres=0; idLettres < nbColones; idLettres++) {
+            System.out.printf(Alphabet[idLettres]+"ㅤ");
+        }
+        System.out.printf("\n");
         for (int idLignes=0; idLignes < nbLignes; idLignes++){
-            for (int idColones=0; idColones < nbColones; idColones++){
+            System.out.printf(idLignes+" ");
+            for (int idColones=00; idColones < nbColones; idColones++){
                 if (Tableau[idLignes][idColones] == 0) {
-                    System.out.printf("⬜");
+                    System.out.printf("⬜ ");
                 }
                 if (Tableau[idLignes][idColones] == 1) {
-                    System.out.printf("⬛");
+                    System.out.printf("⬛ ");
                 }
                 if (Tableau[idLignes][idColones] == 2) {
-                    System.out.printf("\uD83D\uDFE5");
+                    System.out.printf("\uD83D\uDFE5 ");
                 }
                 if (Tableau[idLignes][idColones] == 3) {
-                    System.out.printf("\uD83D\uDFE6");
+                    System.out.printf("\uD83D\uDFE6 ");
                 }
             }
             System.out.printf("\n");
         }
+    }
+    /** Fonction de déroulement du jeu */
+    //Fonction de déroulement de jeu en tant que tel
+    public static void Jeu(int[][] plateau){
+        // Defini les joueurs et leurs attribue une couleurs
+        Joueur joueur1 = new Joueur();
+        joueur1.setPseudo("Rouge");
+        joueur1.setCouleur(2);
+        Joueur joueur2 = new Joueur();
+        joueur2.setPseudo("Bleu");
+        joueur2.setCouleur(3);
+        // On crée un tableau avec les jouers afin d'en choisir un au hasard
+        Joueur[] toutLesJoueurs = new Joueur[]{joueur1, joueur2};
+        Random joueurAleatoire = new Random();
+        // On en choisis un au hasard afin qu'il soit le premier a jouer et on defini le deuxieme joueur en fonction du premier
+        int choixDuJoueurAleatoire = joueurAleatoire.nextInt(toutLesJoueurs.length);
+        Joueur premierAJouer = toutLesJoueurs[choixDuJoueurAleatoire];
+        Joueur deuxiemeAJouer;
+        if (choixDuJoueurAleatoire == 1){
+            deuxiemeAJouer = toutLesJoueurs[0];
+        }else {
+            deuxiemeAJouer = toutLesJoueurs[1];
+        }
+        // On met les coordonnée par default au premier joueur
+        premierAJouer.setCoordoneeX(4);
+        premierAJouer.setCoordoneeY(5);
+        // On met les coordonnée par default au deuxième joueur
+        deuxiemeAJouer.setCoordoneeX(5);
+        deuxiemeAJouer.setCoordoneeY(5);
+        // On change le plateau afin d'y appliquer les coordonnée du joueur et on affiche le plateau
+        plateau[premierAJouer.getCoordoneeX()][premierAJouer.getCoordoneeY()] = premierAJouer.getCouleur();
+        plateau[deuxiemeAJouer.getCoordoneeX()][deuxiemeAJouer.getCoordoneeY()] = deuxiemeAJouer.getCouleur();
+        afficherTableau(plateau);
     }
 }
---
+++ src/classe/Joueur.java
@@ -0,0 +1,58 @@
+package classe;
+/** La Classe des joueurs */
+public class Joueur {
+    int score = 0;
+    String Pseudo;
+    int coordoneeX;
+    int coordoneeY;
+    Boolean enJeu;
+    int couleur;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getPseudo() {
+        return Pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        Pseudo = pseudo;
+    }
+
+    public int getCoordoneeX() {
+        return coordoneeX;
+    }
+
+    public void setCoordoneeX(int coordoneeX) {
+        this.coordoneeX = coordoneeX;
+    }
+
+    public int getCoordoneeY() {
+        return coordoneeY;
+    }
+
+    public void setCoordoneeY(int coordoneeY) {
+        this.coordoneeY = coordoneeY;
+    }
+
+    public Boolean getEnJeu() {
+        return enJeu;
+    }
+
+    public void setEnJeu(Boolean enJeu) {
+        this.enJeu = enJeu;
+    }
+
+    public int getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(int couleur) {
+        this.couleur = couleur;
+    }
+}
