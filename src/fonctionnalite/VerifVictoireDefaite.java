1ab91533cd4ce8bf53dfbc55a85daf36c4fffa97
Gento91 <leo.kontente@gmail.com>
2024-02-16 13:09:17 +0100

    Ajout du multijoueurs

--- src/fonctionnalite/VerifVictoireDefaite.java
+++ src/fonctionnalite/VerifVictoireDefaite.java
@@ -4,13 +4,13 @@ import classe.Joueur;
 
 /** Vérifie si un joueur peut bouger */
 public class VerifVictoireDefaite {
-    public static boolean verifVictoireDefaite(int[][] plateau, Joueur enTrainDeJouer){
+    public static boolean VerifDefaite(int[][] plateau, Joueur enTrainDeJouer){
         if ((enTrainDeJouer.getCoordoneeX()-1 == -1 || plateau[enTrainDeJouer.getCoordoneeX() - 1][enTrainDeJouer.getCoordoneeY()] != 0) &&
                 (enTrainDeJouer.getCoordoneeX()+1 == plateau.length || plateau[enTrainDeJouer.getCoordoneeX() + 1][enTrainDeJouer.getCoordoneeY()] != 0) &&
                 (enTrainDeJouer.getCoordoneeY()-1 == -1 || plateau[enTrainDeJouer.getCoordoneeX()][enTrainDeJouer.getCoordoneeY() - 1] != 0) &&
                 (enTrainDeJouer.getCoordoneeY()+1 == plateau[0].length || plateau[enTrainDeJouer.getCoordoneeX()][enTrainDeJouer.getCoordoneeY() + 1] != 0)){
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 }
